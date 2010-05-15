edb279a7ded9cd64ad6f769c9cbb75132073baa6
Tim Gerundt <tim@gerundt.de>
2010-05-15 14:47:40 +0200

    Add "DllBuild 0028" from 7-Zip Plugin

--- htdocs/downloads/plugins.php
+++ htdocs/downloads/plugins.php
@@ -8,24 +8,25 @@
   
   $page->printHeading(__('Download Plugins'));
   $page->printSubHeading(__('7-Zip Plugin'));
-  $page->printPara(__('This is the 7-Zip plugin for WinMerge and the current version <strong>%1$s</strong> was released at <strong>%2$s</strong>.', 'DllBuild 0026', '2007-12-23'));
+  $page->printPara(__('This is the 7-Zip plugin for WinMerge and the current version <strong>%1$s</strong> was released at <strong>%2$s</strong>.', 'DllBuild 0028', '2010-05-13'));
 ?>
 <div class="downloadmatrix">
 <ul>
   <li><strong><?php __e('Installer');?></strong>
     <ul>
-      <li><a href="http://downloads.sourceforge.net/winmerge/Merge7zInstaller0026-432-457.exe"><?php __e('Exe-Format (%s MB)', '1.05');?></a></li>
+      <li><a href="http://downloads.sourceforge.net/winmerge/Merge7zInstaller0028-465-913.exe"><?php __e('Exe-Format (%s MB)', '0.80');?></a></li>
     </ul>
   </li>
   <li><strong><?php __e('Binaries');?></strong>
     <ul>
-      <li><a href="http://downloads.sourceforge.net/winmerge/Merge7z0026-432-457.7z"><?php __e('7z-Format (%s MB)', '0.91');?></a></li>
+      <li><a href="http://downloads.sourceforge.net/winmerge/Merge7z0028-465-913.7z"><?php __e('7z-Format (%s MB)', '0.68');?></a></li>
     </ul>
   </li>
 </ul>
 </div>
 <?php
-  $page->printPara(__('You can also read the <a href="%1$s">Release Notes</a> or search for <a href="%2$s">other versions</a>.', 'http://sourceforge.net/project/shownotes.php?release_id=563695&amp;group_id=13216', 'http://sourceforge.net/project/showfiles.php?group_id=13216&amp;package_id=143957'));
+  //$page->printPara(__('You can also read the <a href="%1$s">Release Notes</a> or search for <a href="%2$s">other versions</a>.', 'http://sourceforge.net/project/shownotes.php?release_id=563695&amp;group_id=13216', 'http://sourceforge.net/project/showfiles.php?group_id=13216&amp;package_id=143957'));
+  $page->printPara(__('You can also search for <a href="%s">other versions</a>.', 'http://sourceforge.net/projects/winmerge/files/3.%207-Zip%20plugin/'));
   
   $page->printSubHeading(__('xdocdiff Plugin'));
   $page->printPara(__('With this plugin you can compare Word, Excel, PowerPoint, PDF and some more files.'));
