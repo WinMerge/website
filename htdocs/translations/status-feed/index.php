c863fd968005fdbf339d3672d23aec22a9ec23f6
Tim Gerundt <tim@gerundt.de>
2018-06-21 22:16:17 +0200

    Add Website translations status to RSS feed

--- htdocs/translations/status-feed/index.php
+++ htdocs/translations/status-feed/index.php
@@ -2,13 +2,23 @@
   ob_start('ob_gzhandler'); //Use GZIP compression from PHP, since 1and1 don't support it from Apache!
   header('Content-type: application/rss+xml');
   
+  include('../../engine/engine.inc');
   include('../translations.inc');
   
   try {
-    $status = New TranslationsStatus('../status_trunk.xml');
-    $status->srcUrl = 'https://bitbucket.org/winmerge/winmerge/src/default/Translations/';
-    $status->rssLink = 'http://winmerge.org/translations/status-feed/';
-    $status->printRSS();
+    $status = New TranslationsStatus('../status.xml');
+    $webStatus = New TranslationsStatus('../status-website.xml');
+    print("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n");
+    print("<rss version=\"2.0\">\n");
+    print("  <channel>\n");
+    print("    <title>WinMerge Translations Status</title>\n");
+    print("    <link>http://winmerge.org/</link>\n");
+    print("    <description>...</description>\n");
+    $status->printProjectRssItem('WinMerge', 'http://winmerge.org/translations/#winmerge', 'https://bitbucket.org/winmerge/winmerge/src/default/Translations/WinMerge/');
+    $status->printProjectRssItem('ShellExtension', 'http://winmerge.org/translations/#shellextension', 'https://bitbucket.org/winmerge/winmerge/src/default/Translations/ShellExtension/');
+    $status->printProjectRssItem('InnoSetup', 'http://winmerge.org/translations/#innosetup', 'https://bitbucket.org/winmerge/winmerge/src/default/Translations/InnoSetup/');
+    $status->printProjectRssItem('Docs/Readme', 'http://winmerge.org/translations/#readme', 'https://bitbucket.org/winmerge/winmerge/src/default/Translations/Docs/Readme/');
+    $webStatus->printProjectRssItem('Website', 'http://winmerge.org/translations/status_website.php', 'https://bitbucket.org/winmerge/website/src/default/po/');
   }
   catch (Exception $ex) { //If problems with translations status...
     print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
