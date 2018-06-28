a68b0d7439a633d556ecab256b8b589c74664c15
Tim Gerundt <tim@gerundt.de>
2018-06-28 23:02:39 +0200

    CreateMasterPotFile.py works now with Python 3

--- po/CreateMasterPotFile.py
+++ po/CreateMasterPotFile.py
@@ -3,39 +3,32 @@
 
 # Python script to create the POT file for the website
 #
-# Copyright 2009-2010 Tim Gerundt <tim@gerundt.de>
+# Copyright 2009-2018 Tim Gerundt <tim@gerundt.de>
 #
 # This file is part of WinMerge. WinMerge is free software under the terms of the
 # GNU General Public License. You should have received a copy of the license
 # along with WinMerge.  If not, see <http://www.gnu.org/licenses/>.
 
-from os import sep
-from os.path import abspath, isfile, join, splitext, walk
+import os
 from time import strftime
-import string
 import re
 
 def getPhpFiles(path):
     ''' Get all php files from a folder and his subfolders '''
     phpfiles = []
-    walk(path, walkPhpFiles, phpfiles)
+    for root, dirs, files in os.walk(path, topdown = True):
+      for file in files:
+        basename, extension = os.path.splitext(file)
+        if str.lower(extension) == '.php' or str.lower(extension) == '.inc': #If a PHP file...
+          phpfiles.append(os.path.join(root, file))
     return phpfiles
-
-def walkPhpFiles(phpfiles, dirpath, itemnames):
-    ''' Helper function for getPhpFiles() '''
-    for itemname in itemnames: #For all dir items...
-        fullitempath = abspath(join(dirpath, itemname))
-        if isfile(fullitempath): #If a file...
-            filename = splitext(itemname)
-            if str.lower(filename[1]) == '.php' or str.lower(filename[1]) == '.inc': #If a PHP file...
-                phpfiles.append(fullitempath)
 
 def getTranslationsFromPhpFile(filepath, translations):
     ''' Get the translations from a php file '''
     rGettext = re.compile('_e?\([\'](.+?)[\']', re.DOTALL)
     rGettextTestMultiLine = re.compile('_e?\([\']([^\'\n]+)\n')
     
-    phpfile = open(filepath, 'r')
+    phpfile = open(filepath, 'r', errors='ignore')
     lines = phpfile.readlines()
     phpfile.close()
     
@@ -48,8 +41,8 @@ def getTranslationsFromPhpFile(filepath, translations):
         if tmps: #If found a multi-line gettext function...
             tmps = rGettext.findall("".join(lines[i:]))
             for tmp in tmps: #For all translations...
-                if string.find(tmp, '\n') > 0: #If a multi-line translation...
-                    translation = string.replace(tmp, '\n', '\\n')
+                if tmp.find('\n') > 0: #If a multi-line translation...
+                    translation = tmp.replace('\n', '\\n')
                     if translation in translations: #If the translation is already exists...
                         translations[translation] += [(filepath, i)]
                     else: #If the translation is NOT already exists...
@@ -73,31 +66,31 @@ def getTranslationsFromPhpFile(filepath, translations):
 
 def main():
     translations = {}
-    php_dir = abspath('../htdocs')
+    php_dir = os.path.abspath('../htdocs')
     php_files = getPhpFiles(php_dir)
     php_files.sort()
     for php_file in php_files: #For all php files...
         getTranslationsFromPhpFile(php_file, translations)
     
     potfile = open('en-US.pot', 'wb') #Open as binary to prevent the translation of EOL characters!
-    potfile.write('# This file is part from WinMerge <http://winmerge.org/>\n')
-    potfile.write('# Released under the "GNU General Public License"\n')
-    potfile.write('# \n')
-    potfile.write('msgid ""\n')
-    potfile.write('msgstr ""\n')
-    potfile.write('"Project-Id-Version: WinMerge\\n"\n')
-    potfile.write('"Report-Msgid-Bugs-To: http://bugs.winmerge.org\\n"\n')
-    potfile.write('"POT-Creation-Date: %s\\n"\n' % strftime('%Y-%m-%d %H:%M+0000'))
-    potfile.write('"PO-Revision-Date: \\n"\n')
-    potfile.write('"Last-Translator: \\n"\n')
-    potfile.write('"Language-Team: English <winmerge-translate@lists.sourceforge.net>\\n"\n')
-    potfile.write('"MIME-Version: 1.0\\n"\n')
-    potfile.write('"Content-Type: text/plain; charset=utf-8\\n"\n')
-    potfile.write('"Content-Transfer-Encoding: 8bit\\n"\n')
-    potfile.write('"X-Poedit-Language: English\\n"\n')
-    potfile.write('"X-Poedit-SourceCharset: utf-8\\n"\n')
-    potfile.write('"X-Poedit-Basepath: ../htdocs/\\n"\n')
-    potfile.write('\n')
+    potfile.write(b'# This file is part from WinMerge <http://winmerge.org/>\n')
+    potfile.write(b'# Released under the "GNU General Public License"\n')
+    potfile.write(b'# \n')
+    potfile.write(b'msgid ""\n')
+    potfile.write(b'msgstr ""\n')
+    potfile.write(b'"Project-Id-Version: WinMerge\\n"\n')
+    potfile.write(b'"Report-Msgid-Bugs-To: http://bugs.winmerge.org\\n"\n')
+    potfile.write(b'"POT-Creation-Date: %s\\n"\n' % strftime('%Y-%m-%d %H:%M+0000').encode())
+    potfile.write(b'"PO-Revision-Date: \\n"\n')
+    potfile.write(b'"Last-Translator: \\n"\n')
+    potfile.write(b'"Language-Team: English <winmerge-translate@lists.sourceforge.net>\\n"\n')
+    potfile.write(b'"MIME-Version: 1.0\\n"\n')
+    potfile.write(b'"Content-Type: text/plain; charset=utf-8\\n"\n')
+    potfile.write(b'"Content-Transfer-Encoding: 8bit\\n"\n')
+    potfile.write(b'"X-Poedit-Language: English\\n"\n')
+    potfile.write(b'"X-Poedit-SourceCharset: utf-8\\n"\n')
+    potfile.write(b'"X-Poedit-Basepath: ../htdocs/\\n"\n')
+    potfile.write(b'\n')
     for translation in translations: #For all translations...
         #--------------------------------------------------------------------------------
         # References...
@@ -105,24 +98,24 @@ def main():
         reference_line = ''
         references = translations[translation]
         for reference in references: #For all references...
-            reference_path = reference[0].replace(php_dir + sep, '') #Switch to relative path
+            reference_path = reference[0].replace(php_dir + os.sep, '') #Switch to relative path
             reference_path = reference_path.replace('\\', '/') #Switch to linux path separator
             reference_id = '%s:%u' % (reference_path, reference[1])
             if reference_line: #If NOT first reference...
                 if len(reference_line + reference_id) > 77: #If reference line to long...
-                    potfile.write('%s\n' % reference_line)
+                    potfile.write(b'%s\n' % reference_line.encode())
                     reference_line = '#: ' + reference_id
                 else: #If reference line NOT to long...
                     reference_line += ' ' + reference_id
             else: #If first reference...
                 reference_line = '#: ' + reference_id
         if reference_line: #If reference line exists...
-            potfile.write('%s\n' % reference_line)
+            potfile.write(b'%s\n' % reference_line.encode())
         #--------------------------------------------------------------------------------
-        potfile.write('#, c-format\n')
-        potfile.write('msgid "%s"\n' % (translation.replace('"', '\\"')))
-        potfile.write('msgstr ""\n')
-        potfile.write('\n')
+        potfile.write(b'#, c-format\n')
+        potfile.write(b'msgid "%s"\n' % translation.replace('"', '\\"').encode())
+        potfile.write(b'msgstr ""\n')
+        potfile.write(b'\n')
     potfile.close()
 
 # MAIN #
