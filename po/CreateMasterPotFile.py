b45e52b358940d805e359c2608419e910dc11b50
Tim Gerundt <tim@gerundt.de>
2018-07-07 00:06:19 +0200

    Use io.open() in CreateMasterPotFile.py

--- po/CreateMasterPotFile.py
+++ po/CreateMasterPotFile.py
@@ -9,7 +9,7 @@
 # GNU General Public License. You should have received a copy of the license
 # along with WinMerge.  If not, see <http://www.gnu.org/licenses/>.
 
-import codecs
+import io
 import os
 from time import strftime
 import re
@@ -29,7 +29,7 @@ def getTranslationsFromPhpFile(filepath, translations):
     rGettext = re.compile('_e?\([\'](.+?)[\']', re.DOTALL)
     rGettextTestMultiLine = re.compile('_e?\([\']([^\'\n]+)\n')
     
-    phpfile = codecs.open(filepath, 'r', 'utf-8', errors='ignore')
+    phpfile = io.open(filepath, 'r', encoding='utf-8', errors='ignore')
     lines = phpfile.readlines()
     phpfile.close()
     
@@ -74,25 +74,25 @@ def main():
     for php_file in php_files: #For all php files...
         getTranslationsFromPhpFile(php_file, translations)
     
-    potfile = open('en-US.pot', 'wb') #Open as binary to prevent the translation of EOL characters!
-    potfile.write(b'# This file is part from WinMerge <http://winmerge.org/>\n')
-    potfile.write(b'# Released under the "GNU General Public License"\n')
-    potfile.write(b'# \n')
-    potfile.write(b'msgid ""\n')
-    potfile.write(b'msgstr ""\n')
-    potfile.write(b'"Project-Id-Version: WinMerge\\n"\n')
-    potfile.write(b'"Report-Msgid-Bugs-To: http://bugs.winmerge.org\\n"\n')
-    potfile.write(b'"POT-Creation-Date: %s\\n"\n' % strftime('%Y-%m-%d %H:%M+0000').encode())
-    potfile.write(b'"PO-Revision-Date: \\n"\n')
-    potfile.write(b'"Last-Translator: \\n"\n')
-    potfile.write(b'"Language-Team: English <winmerge-translate@lists.sourceforge.net>\\n"\n')
-    potfile.write(b'"MIME-Version: 1.0\\n"\n')
-    potfile.write(b'"Content-Type: text/plain; charset=utf-8\\n"\n')
-    potfile.write(b'"Content-Transfer-Encoding: 8bit\\n"\n')
-    potfile.write(b'"X-Poedit-Language: English\\n"\n')
-    potfile.write(b'"X-Poedit-SourceCharset: utf-8\\n"\n')
-    potfile.write(b'"X-Poedit-Basepath: ../htdocs/\\n"\n')
-    potfile.write(b'\n')
+    potfile = io.open('en-US.pot', 'w', encoding='utf-8', errors='ignore', newline='\n')
+    potfile.write('# This file is part from WinMerge <http://winmerge.org/>\n')
+    potfile.write('# Released under the "GNU General Public License"\n')
+    potfile.write('# \n')
+    potfile.write('msgid ""\n')
+    potfile.write('msgstr ""\n')
+    potfile.write('"Project-Id-Version: WinMerge\\n"\n')
+    potfile.write('"Report-Msgid-Bugs-To: http://bugs.winmerge.org\\n"\n')
+    potfile.write('"POT-Creation-Date: %s\\n"\n' % strftime('%Y-%m-%d %H:%M+0000'))
+    potfile.write('"PO-Revision-Date: \\n"\n')
+    potfile.write('"Last-Translator: \\n"\n')
+    potfile.write('"Language-Team: English <winmerge-translate@lists.sourceforge.net>\\n"\n')
+    potfile.write('"MIME-Version: 1.0\\n"\n')
+    potfile.write('"Content-Type: text/plain; charset=utf-8\\n"\n')
+    potfile.write('"Content-Transfer-Encoding: 8bit\\n"\n')
+    potfile.write('"X-Poedit-Language: English\\n"\n')
+    potfile.write('"X-Poedit-SourceCharset: utf-8\\n"\n')
+    potfile.write('"X-Poedit-Basepath: ../htdocs/\\n"\n')
+    potfile.write('\n')
     for translation in translations: #For all translations...
         #--------------------------------------------------------------------------------
         # References...
@@ -105,19 +105,19 @@ def main():
             reference_id = '%s:%u' % (reference_path, reference[1])
             if reference_line: #If NOT first reference...
                 if len(reference_line + reference_id) > 77: #If reference line to long...
-                    potfile.write(b'%s\n' % reference_line.encode())
+                    potfile.write('%s\n' % reference_line)
                     reference_line = '#: ' + reference_id
                 else: #If reference line NOT to long...
                     reference_line += ' ' + reference_id
             else: #If first reference...
                 reference_line = '#: ' + reference_id
         if reference_line: #If reference line exists...
-            potfile.write(b'%s\n' % reference_line.encode())
+            potfile.write('%s\n' % reference_line)
         #--------------------------------------------------------------------------------
-        potfile.write(b'#, c-format\n')
-        potfile.write(b'msgid "%s"\n' % translation.replace('"', '\\"').encode())
-        potfile.write(b'msgstr ""\n')
-        potfile.write(b'\n')
+        potfile.write('#, c-format\n')
+        potfile.write('msgid "%s"\n' % translation.replace('"', '\\"'))
+        potfile.write('msgstr ""\n')
+        potfile.write('\n')
     potfile.close()
 
 # MAIN #
