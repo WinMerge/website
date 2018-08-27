cbe2a6a681df8c867c722d65ca63e487772b1c1e
Tim Gerundt <tim@gerundt.de>
2018-08-27 21:25:28 +0200

    Add "Features" to Home page

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,15 +11,55 @@
   
   $page->printHeading(__('What is WinMerge?'));
   $page->printPara(__('WinMerge is an <a href="%s">Open Source</a> differencing and merging tool for Windows. WinMerge can compare both folders and files, presenting differences in a visual text format that is easy to understand and handle.', 'source-code/'));
-?>
-<p><strong><?php __e('<a href="%1$s">Learn More</a> or <a href="%2$s">Download Now!</a>', 'about/', 'downloads/');?></strong></p>
-<?php
+
   $page->printSubHeading(__('Screenshot'));
 ?>
 <p><img class="image" src="about/screenshots/screenshot.png" alt="<?php __e('File Comparison');?>" width="792" height="519" border="0"></p>
 <?php
   $page->printPara(__('See the <a href="%s">screenshots page</a> for more screenshots.', 'about/screenshots/'));
 
+  $page->printSubHeading(__('Features'));
+  $page->printPara(__('WinMerge is highly useful for determining what has changed between project versions, and then merging changes between versions. WinMerge can be used as an external differencing/merging tool or as a standalone application.'));
+  $page->printPara(__('In addition, WinMerge has many helpful supporting features that make comparing, synchronising, and merging as easy and useful as possible:'));
+?>
+<h4><?php __e('General');?></h4>
+<ul>
+  <li><?php __e('Supports Microsoft Windows 2000/XP/2003/Vista/2008/7/8/2012');?></li>
+  <li><?php __e('Handles Windows, Unix and Mac text file formats');?></li>
+  <li><?php __e('Unicode support');?></li>
+  <li><?php __e('Tabbed interface');?></li>
+</ul>
+<h4><?php __e('File Compare');?></h4>
+<ul>
+  <li><?php __e('Visual differencing and merging of text files');?></li>
+  <li><?php __e('Flexible editor with syntax highlighting, line numbers and word-wrap');?></li>
+  <li><?php __e('Highlights differences inside lines');?></li>
+  <li><?php __e('Difference pane shows current difference in two vertical panes');?></li>
+  <li><?php __e('Location pane shows map of files compared');?></li>
+  <li><?php __e('Moved lines detection');?></li>
+</ul>
+<h4><?php __e('Folder Compare');?></h4>
+<ul>
+  <li><?php __e('Regular Expression based file filters allow excluding and including items');?></li>
+  <li><?php __e('Fast compare using file sizes and dates');?></li>
+  <li><?php __e('Compares one folder or includes all subfolders');?></li>
+  <li><?php __e('Can show folder compare results in a tree-style view');?></li>
+</ul>
+<h4><?php __e('Version Control');?></h4>
+<ul>
+  <li><?php __e('Creates patch files (Normal-, Context- and Unified formats)');?></li>
+  <li><?php __e('Resolve conflict files');?></li>
+  <li><?php __e('Rudimentary Visual SourceSafe and Rational ClearCase integration');?></li>
+</ul>
+<h4><?php __e('Other');?></h4>
+<ul>
+  <li><?php __e('Shell Integration (supports 64-bit Windows versions)');?></li>
+  <li><?php __e('Archive file support using 7-Zip');?></li>
+  <li><?php __e('Plugin support');?></li>
+  <li><?php __e('Localizable interface');?></li>
+  <li><?php __e('<a href="%s">Online manual</a> and installed HTML Help manual', 'http://manual.winmerge.org/');?></li>
+</ul>
+<?php
   $page->printSubHeading(__('WinMerge %s - latest stable version', $stablerelease->getVersionNumber()));
   $page->printPara(__('<a href="%1$s">WinMerge %2$s</a> is the latest stable version, and is recommended for most users.', 'downloads/', $stablerelease->getVersionNumber()));
   $page->printDownloadNow();
