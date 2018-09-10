77097d319db14758ee9c6cefd60fc2a643774d70
Tim Gerundt <tim@gerundt.de>
2018-09-10 05:28:37 +0200

    Don't use info cards for mailing lists

--- htdocs/support/index.php
+++ htdocs/support/index.php
@@ -35,63 +35,46 @@
 
   $page->printSubHeading(__('Mailing Lists'));
 ?>
-<ul>
-  <li><a href="#announce"><?php __e('Announce List');?></a></li>
-  <li><a href="#support"><?php __e('Support List');?></a></li>
-  <li><a href="#user"><?php __e('User List');?></a></li>
-  <li><a href="#translate"><?php __e('Translate List');?></a></li>
-  <li><a href="#development"><?php __e('Development List');?></a></li>
+<h4><?php __e('Announce List');?></h4>
+<p><?php __e('This list is only for announcing new releases of WinMerge.');?></p>
+<ul class="buttons">
+  <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-announce" class="button is-small is-success"><?php __e('Subscribe');?></a></li>
+  <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-announce" class="button is-small is-danger"><?php __e('Unsubscribe');?></a></li>
+  <li><a href="https://sourceforge.net/mailarchive/forum.php?forum_name=winmerge-announce" class="button is-small"><?php __e('Archive');?></a></li>
+  <li><strong><?php __e('Low Traffic');?></strong></li>
 </ul>
-<div class="infocard">
-  <h3><a name="announce"><?php __e('Announce List');?></a></h3>
-  <ul>
-    <li><strong><?php __e('Low Traffic');?></strong></li>
-    <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-announce"><?php __e('Subscribe');?></a></li>
-    <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-announce"><?php __e('Unsubscribe');?></a></li>
-    <li><a href="https://sourceforge.net/mailarchive/forum.php?forum_name=winmerge-announce"><?php __e('Archive');?></a></li>
-  </ul>
-  <p><?php __e('This list is only for announcing new releases of WinMerge.');?></p>
-</div>
-<div class="infocard">
-  <h3><a name="support"><?php __e('Support List');?></a></h3>
-  <ul>
-    <li><strong><?php __e('Normal Traffic');?></strong></li>
-    <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-support"><?php __e('Subscribe');?></a></li>
-    <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-support"><?php __e('Unsubscribe');?></a></li>
-    <li><a href="https://sourceforge.net/mailarchive/forum.php?forum_name=winmerge-support"><?php __e('Archive');?></a></li>
-  </ul>
-  <p><?php __e('You should ask all your support questions on this list.');?></p>
-</div>
-<div class="infocard">
-  <h3><a name="user"><?php __e('User List');?></a></h3>
-  <ul>
-    <li><strong><?php __e('Normal Traffic');?></strong></li>
-    <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-user"><?php __e('Subscribe');?></a></li>
-    <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-user"><?php __e('Unsubscribe');?></a></li>
-    <li><a href="https://sourceforge.net/mailarchive/forum.php?forum_name=winmerge-user"><?php __e('Archive');?></a></li>
-  </ul>
-  <p><?php __e('This list is for user related questions. Having questions posted to this list may allow others having the same problem to solve it on their own.');?></p>
-</div>
-<div class="infocard">
-  <h3><a name="translate"><?php __e('Translate List');?></a></h3>
-  <ul>
-    <li><strong><?php __e('Low Traffic');?></strong></li>
-    <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-translate"><?php __e('Subscribe');?></a></li>
-    <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-translate"><?php __e('Unsubscribe');?></a></li>
-    <li><a href="https://sourceforge.net/mailarchive/forum.php?forum_name=winmerge-translate"><?php __e('Archive');?></a></li>
-  </ul>
-  <p><?php __e('This list is used for coordinating the translations.');?></p>
-</div>
-<div class="infocard">
-  <h3><a name="development"><?php __e('Development List');?></a></h3>
-  <ul>
-    <li><strong><?php __e('Normal Traffic');?></strong></li>
-    <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-development"><?php __e('Subscribe');?></a></li>
-    <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-development"><?php __e('Unsubscribe');?></a></li>
-    <li><a href="https://sourceforge.net/mailarchive/forum.php?forum_name=winmerge-development"><?php __e('Archive');?></a></li>
-  </ul>
-  <p><?php __e('This is the list where participating developers of the WinMerge meet and discuss issues, code changes/additions, etc.');?></p>
-</div>
+<h4><?php __e('Support List');?></em></h4>
+<p><?php __e('You should ask all your support questions on this list.');?></p>
+<ul class="buttons">
+  <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-support" class="button is-small is-success"><?php __e('Subscribe');?></a></li>
+  <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-support" class="button is-small is-danger"><?php __e('Unsubscribe');?></a></li>
+  <li><a href="https://sourceforge.net/mailarchive/forum.php?forum_name=winmerge-support" class="button is-small"><?php __e('Archive');?></a></li>
+  <li><strong><?php __e('Normal Traffic');?></strong></li>
+</ul>
+<h4><?php __e('User List');?></h4>
+<p><?php __e('This list is for user related questions. Having questions posted to this list may allow others having the same problem to solve it on their own.');?></p>
+<ul class="buttons">
+  <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-user" class="button is-small is-success"><?php __e('Subscribe');?></a></li>
+  <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-user" class="button is-small is-danger"><?php __e('Unsubscribe');?></a></li>
+  <li><a href="https://sourceforge.net/mailarchive/forum.php?forum_name=winmerge-user" class="button is-small"><?php __e('Archive');?></a></li>
+  <li><strong><?php __e('Normal Traffic');?></strong></li>
+</ul>
+<h4><?php __e('Translate List');?></h4>
+<p><?php __e('This list is used for coordinating the translations.');?></p>
+<ul class="buttons">
+  <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-translate" class="button is-small is-success"><?php __e('Subscribe');?></a></li>
+  <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-translate" class="button is-small is-danger"><?php __e('Unsubscribe');?></a></li>
+  <li><a href="https://sourceforge.net/mailarchive/forum.php?forum_name=winmerge-translate" class="button is-small"><?php __e('Archive');?></a></li>
+  <li><strong><?php __e('Low Traffic');?></strong></li>
+</ul>
+<h4><?php __e('Development List');?></h4>
+<p><?php __e('This is the list where participating developers of the WinMerge meet and discuss issues, code changes/additions, etc.');?></p>
+<ul class="buttons">
+  <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-development" class="button is-small is-success"><?php __e('Subscribe');?></a></li>
+  <li><a href="http://lists.sourceforge.net/mailman/listinfo/winmerge-development" class="button is-small is-danger"><?php __e('Unsubscribe');?></a></li>
+  <li><a href="https://sourceforge.net/mailarchive/forum.php?forum_name=winmerge-development" class="button is-small"><?php __e('Archive');?></a></li>
+  <li><strong><?php __e('Normal Traffic');?></strong></li>
+</ul>
 <?php
   $page->printSubHeading(__('Donate'));
   $page->printPara(__('Since WinMerge is an <a href="%s">Open Source</a> project, you may use it free of charge.', '/source-code/'),
