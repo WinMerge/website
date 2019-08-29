7cd4a0df2cea8068e2aa085e6b27e56539d6d2af
Tim Gerundt <tim@gerundt.de>
2019-08-29 06:06:02 +0200

    Show only Git and recents commits on "Source Code" page

--- htdocs/source-code/index.php
+++ htdocs/source-code/index.php
@@ -1,9 +1,13 @@
 <?php
   include('../engine/engine.inc');
+  include('../engine/simplepie/simplepie.inc');
+
+  $recentCommitsFeed = 'https://github.com/WinMerge/winmerge/commits/master.atom';
 
   $page = new Page;
   $page->setDescription(__('Download the source code of WinMerge, which is released under the GNU General Public License.'));
-  $page->setKeywords(__('WinMerge, free, download, source code, GPL, Bitbucket, Mercurial'));
+  $page->setKeywords(__('WinMerge, free, clone, download, source code, GPL, GitHub, Git'));
+  $page->addRssFeed($recentCommitsFeed, __('Recent Commits'));
   $page->printHead(__('Source Code'), TAB_SOURCE_CODE);
   
   $page->printHeading(__('Source Code'));
@@ -28,13 +32,23 @@ You should have received a copy of the GNU General Public License
 along with WinMerge.  If not, see &lt;<a href="https://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>&gt;.</pre>
 <?php
   $page->printAnchorSubHeading(__('Clone or download'), 'clone-or-download');
-  $page->printPara(__('The source code is hosted on <a href="%1$s">Bitbucket</a> in a <a href="%2$s">Mercurial</a> repository.', 'https://bitbucket.org/', 'https://www.mercurial-scm.org/'),
-                   __('We have also a <a href="%1$s">Git</a> repository as mirror on <a href="%2$s">GitHub</a>.', 'https://git-scm.com/', 'https://github.com/'));
+  $page->printPara(__('The source code is hosted on <a href="%1$s">GitHub</a> in a <a href="%2$s">Git</a> repository:', 'https://github.com/', 'https://git-scm.com/'));
+  $page->printPara('<a href="https://github.com/winmerge/winmerge.git" class="button is-small">https://github.com/winmerge/winmerge.git</a>');
 
-  $page->printSubSubHeading(__('Mercurial'));
-  $page->printPara('<a href="https://bitbucket.org/winmerge/winmerge" class="button is-small">https://bitbucket.org/winmerge/winmerge</a>');
-  $page->printSubSubHeading(__('Git Mirror'));
-  $page->printPara('<a href="https://github.com/winmerge/winmerge.git" class="button is-small is-light">https://github.com/winmerge/winmerge.git</a>');
+  $page->printRssSubHeading(__('Recent Commits'), $recentCommitsFeed);
+  $feed = new SimplePie();
+  $feed->set_feed_url($recentCommitsFeed);
+  $feed->set_cache_location('../engine/simplepie/cache');
+  $feed->init();
+  print("<ul class=\"rssfeeditems\">\n");
+  foreach ($feed->get_items(0, 10) as $item) { //for the last 10 commits...
+    $title = strip_tags($item->get_title());
+    $link = $item->get_link();
+    $date = $item->get_date(__('Y-m-d H:i'));
+    print("  <li><a href=\"$link\">$title</a> <em>$date</em></li>\n");
+  }
+  print("  <li><a href=\"https://github.com/WinMerge/winmerge/commits/master\">" . __('View all commits&hellip;') . "</a></li>\n");
+  print("</ul>\n");
 
   $page->printFoot();
 ?>
